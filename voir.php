ebd779fa839edbf48172f56f10204685c5e68125
Yoel Melki <ymelki@gmail.com>
2022-05-24 12:33:13 +0200

    test

--- voir.php
+++ voir.php
@@ -1,9 +1,13 @@
-<?php include "header.php";     ?>
-<?php
+<?php include "header.php";     ?>    
 
-      
-// Récupère les données de la table produits
-$requete1 = 'SELECT * FROM produit';
+      <p class="h1">Bienvenue sur notre site</p>
+
+ Afficher un produit spécifique 
+<?php
+ // Récupère les données de la table produits
+ $id=$_GET['id'];
+ echo $id;
+$requete1 = "SELECT * FROM produit WHERE  id=".$id;
 
 // le prepare (avec le execute) est comme un query mais beaucoup plus sécurisé (voir ci-dessous)
 // l'opérateur flèche -> permer d'accéder aux éléments d'une classe (méthode ou attribut)
@@ -12,10 +16,6 @@ $resultat->execute();
 
 
 ?>
-
-      <p class="h1">Bienvenue sur notre site</p>
-
-
 
 <div class="row row-cols-1 row-cols-md-3 g-4">
 
@@ -27,7 +27,7 @@ $resultat->execute();
     <div class="card h-100">
       <img src="..." class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title"> <a href=voir.php?id=<?=$ligne['id']?> >  <?= $ligne['nom']."</a> ". $ligne['prix']." € "?></h5>
+        <h5 class="card-title"><?= $ligne['nom']." ". $ligne['prix']." € "?></h5>
         <p class="card-text"><?= $ligne['description'] ?></p>
       </div>
     </div>
@@ -37,16 +37,5 @@ $resultat->execute();
 	}  
 
   ?>
-  
-</div>
-
-
-
-
-
-   
-
-   
-
-</body>
-</html>
+      
+<?php include "footer.php";     ?>    
